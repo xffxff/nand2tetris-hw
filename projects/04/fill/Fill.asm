8e7b77c342b1319a183f46fba884fc404028ed63
xffxff <1247714429@qq.com>
2021-02-04 18:24:06 +0800

    has a bug

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,76 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+@i
+M=1 //i = 1
+
+// n = 8192 (32 * 256)
+@8192
+D=A
+@n
+M=D 
+
+// screen = SCREEN
+@SCREEN
+D=A
+@screen
+M=D
+
+(BLACK)
+@24576
+D=M
+@WHITE
+D;JGT
+
+@i
+D=M
+@n
+D=D-M
+@END
+D;JGT
+
+@screen
+A=M
+M=-1
+@i
+M=M+1
+
+@screen
+M=M+1
+@BLACK
+0;JMP
+
+(WHITE)
+@24576
+D=M
+@BLACK
+D;JEQ
+
+@i
+D=M
+@n
+D=D-M
+@END
+D;JGT
+
+@screen
+A=M
+M=0
+@i
+M=M+1
+
+@screen
+M=M+1
+@BLACK
+0;JMP
+
+(END)
+@i
+M=1
+@SCREEN
+D=A
+@screen
+M=D
+@LOOP
+0;JMP
